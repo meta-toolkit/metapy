53244005c03ade3f630e401cb6db8e145b306c96
Chase Geigle <sky@skystrife.com>
2016-04-19 17:49:42 -0500

    Update cmake in travis OS X builds.

--- travis/install_osx.sh
+++ travis/install_osx.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+brew update
+brew outdated cmake || brew upgrade cmake
 git clone https://github.com/MacPython/terryfy
 source terryfy/travis_tools.sh
 get_python_environment macpython $VERSION venv
