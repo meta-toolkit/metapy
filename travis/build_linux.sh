ed935a722b5cf0f7216041c43e1c78f1cd913cca
Chase Geigle <sky@skystrife.com>
2016-04-19 19:16:34 -0500

    Pass UNICODE_WIDTH environment var to docker in Linux wheel builds.

--- travis/build_linux.sh
+++ travis/build_linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo docker run --rm \
     -e PYTHON_VERSION=$VERSION \
+    -e UNICODE_WIDTH=$UNICODE_WIDTH \
     -v `pwd`:/metapy \
     quay.io/pypa/manylinux1_x86_64 /metapy/travis/build_linux_wheel.sh
