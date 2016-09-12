dfc49aff03f7679eef4cd41dbcc1918bb7c40fd3
Chase Geigle <sky@skystrife.com>
2016-09-12 18:38:01 -0500

    Bump version to v0.0.1.dev8.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from distutils.spawn import find_executable
 from distutils import log, sysconfig
 from distutils.command import build
 
-VERSION = "0.0.1.dev7"
+VERSION = "0.0.1.dev8"
 
 # See:
 # http://stackoverflow.com/questions/3223604/how-to-create-a-temporary-directory-and-get-the-path-file-name-in-python
