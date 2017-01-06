4558c9847a9bba68887b2a0404b182e4909c4291
Chase Geigle <geigle1@illinois.edu>
2017-01-06 15:59:00 -0600

    Update travis-ci configuration to build wheel for Python 3.6.

--- travis/install_osx.sh
+++ travis/install_osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 brew update
 brew outdated cmake || brew upgrade cmake
-git clone https://github.com/MacPython/terryfy
+git clone --recursive https://github.com/MacPython/terryfy
 source terryfy/travis_tools.sh
 get_python_environment macpython $VERSION venv
 pip install wheel
