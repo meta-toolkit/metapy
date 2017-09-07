4ba0532f3997d508a6f6b41e603b5e79ade4cffe
Chase Geigle <sky@skystrife.com>
2017-09-06 22:21:33 -0500

    [release]: Bump version number

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from distutils.spawn import find_executable
 from distutils import log, sysconfig
 from distutils.command import build
 
-VERSION = "0.2.7"
+VERSION = "0.2.8"
 
 # See:
 # http://stackoverflow.com/questions/3223604/how-to-create-a-temporary-directory-and-get-the-path-file-name-in-python
