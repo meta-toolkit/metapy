fef345a0493deaad831e1289480b1c01ee4b7f2c
Chase Geigle <geigle1@illinois.edu>
2018-04-30 15:01:43 -0500

    [topics]: Remove unused function.

--- src/metapy_topics.cpp
+++ src/metapy_topics.cpp
@@ -10,30 +10,20 @@
 #include "meta/learn/dataset.h"
 #include "meta/logging/logger.h"
 #include "meta/topics/bl_term_scorer.h"
+#include "meta/topics/inferencer.h"
 #include "meta/topics/lda_cvb.h"
+#include "meta/topics/lda_cvb_inferencer.h"
 #include "meta/topics/lda_gibbs.h"
+#include "meta/topics/lda_gibbs_inferencer.h"
 #include "meta/topics/lda_scvb.h"
 #include "meta/topics/parallel_lda_gibbs.h"
+#include "meta/util/random.h"
+#include "metapy_identifiers.h"
 #include "metapy_stats.h"
 #include "metapy_topics.h"
 
 namespace py = pybind11;
 using namespace meta;
-
-template <class Model>
-void run_lda(const std::string& config_path, const std::string& out_prefix,
-             std::size_t num_topics, double alpha, double beta,
-             std::size_t num_iters, double convergence)
-{
-    logging::set_cerr_logging();
-    auto cfg = cpptoml::parse_file(config_path);
-    auto f_idx = index::make_index<index::forward_index>(*cfg);
-    auto doc_list = f_idx->docs();
-    learn::dataset docs{f_idx, doc_list.begin(), doc_list.end()};
-    Model model{docs, num_topics, alpha, beta};
-    model.run(num_iters, convergence);
-    model.save(out_prefix);
-}
 
 void metapy_bind_topics(py::module& m)
 {
