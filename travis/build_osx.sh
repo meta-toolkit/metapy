450ddb150c779786a607a435b35bb3c601b4048a
Dave McGuire <dmcguire@blackberry.com>
2022-11-12 16:58:26 -0800

    Add fixes for post-macOS-11 (Big Sur) usage of clang

--- travis/build_osx.sh
+++ travis/build_osx.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Apparently, Apple moved the path to these in Big Sur, confusing built-in configuration
+export CLANG_SYSROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"
+export LIBRARY_PATH="${CLANG_SYSROOT}/usr/lib"
+export LINK_FLAGS="-isysroot ${CLANG_SYSROOT}"
+export CFLAGS="-isysroot ${CLANG_SYSROOT}"
+export CXXFLAGS="-isysroot ${CLANG_SYSROOT}"
+export CPPFLAGS="-isysroot ${CLANG_SYSROOT}"
+
 pip wheel -w dist --verbose ./
 ls dist/*.whl
 pip install dist/*.whl
--- travis/install_osx.sh
+++ travis/install_osx.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+# multibuild tooling says "Python should be on the PATH"
 python3 -V
 ln -s /usr/local/bin/python3 /usr/local/bin/python
 export PATH=$PATH:/usr/local/bin
 python -V
-brew update
-brew outdated cmake || brew upgrade cmake
+
+#brew update
+#brew outdated cmake || brew upgrade cmake
 git clone --recursive https://github.com/MacPython/terryfy
 git clone https://github.com/matthew-brett/multibuild
 source multibuild/osx_utils.sh
