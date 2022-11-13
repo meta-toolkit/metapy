6cfb5abc28289a417605e60558ef5ee048c03233
Dave McGuire <dmcguire@blackberry.com>
2022-11-13 09:07:17 -0800

    Update cpython_path logic from manybuild

--- travis/build_linux_wheel.sh
+++ travis/build_linux_wheel.sh
@@ -30,7 +30,7 @@ function strip_dots {
     echo $1 | sed "s/\.//g"
 }
 
-# taken from https://github.com/matthew-brett/manylinux-builds/blob/master/common_vars.sh
+# taken from https://github.com/matthew-brett/multibuild/blob/24d35a4af2583473a0ec224dbe1990dd1e0ace5d/manylinux_utils.sh#L15
 function cpython_path {
     # Return path to cpython given
     # * version (of form "2.7")
@@ -38,20 +38,26 @@ function cpython_path {
     #
     # For back-compatibility "u" as u_width also means "32"
     local py_ver="${1:-2.7}"
+    local abi_suff=m
     local u_width="${2:-${UNICODE_WIDTH}}"
     local u_suff=u
+    # Python 3.8 and up no longer uses the PYMALLOC 'm' suffix
+    # https://github.com/pypa/wheel/pull/303
+    if [ $(lex_ver $py_ver) -ge $(lex_ver 3.8) ]; then
+        abi_suff=""
+    fi
     # Back-compatibility
     if [ "$u_width" == "u" ]; then u_width=32; fi
-    # For Python >= 3.3, "u" suffix not meaningful
-    if [ $(lex_ver $py_ver) -ge $(lex_ver 3.3) ] ||
+    # For Python >= 3.4, "u" suffix not meaningful
+    if [ $(lex_ver $py_ver) -ge $(lex_ver 3.4) ] ||
         [ "$u_width" == "16" ]; then
         u_suff=""
     elif [ "$u_width" != "32" ]; then
         echo "Incorrect u_width value $u_width"
-        # exit 1
+        exit 1
     fi
-    local no_dots=$(strip_dots $py_ver)
-    echo "/opt/python/cp${no_dots}-cp${no_dots}m${u_suff}"
+    local no_dots=$(echo $py_ver | tr -d .)
+    echo "/opt/python/cp${no_dots}-cp${no_dots}$abi_suff${u_suff}"
 }
 
 # taken from https://github.com/matthew-brett/manylinux-builds/blob/master/common_vars.sh
